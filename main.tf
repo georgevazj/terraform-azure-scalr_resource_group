19ed0b5c8b061922601aaa9c73a849adbc57ac1e
n555585 <jorge.vazquezjimenez@gruposantander.es>
2021-06-17 15:35:51 +0200

    Full workload

--- main.tf
+++ main.tf
@@ -11,14 +11,75 @@ provider "azurerm" {
   tenant_id = var.tenant_id
 
 }
+
+locals {
+  header = "sans1weu"
+  rsg_name = "${local.header}rsg${var.workload_acronym}comm${var.sequence_number}"
+  sta_name = "${local.header}sta${var.workload_acronym}comm${var.sequence_number}"
+  akv_name = "${local.header}sta${var.workload_acronym}comm${var.sequence_number}"
+}
+
 resource "azurerm_resource_group" "rsg" {
-  name     = var.name
+  name     = local.rsg_name
   location = var.location
 
   tags = {
     cost_center     = var.cost_center
     product         = var.product
     cia             = var.cia
-
   }
+}
+
+resource "azurerm_key_vault" "akv" {
+  location = var.location
+  name = local.akv_name
+  resource_group_name = azurerm_resource_group.rsg.name
+  sku_name = "premium"
+  tenant_id = var.tenant_id
+
+  tags = {
+    cost_center     = azurerm_resource_group.rsg.tags["cost_center"]
+    product         = azurerm_resource_group.rsg.tags["product"]
+    channel         = var.channel
+    description     = var.description
+    tracking_code   = var.tracking_code
+    cia             = var.cia
+  }
+
+  depends_on = [azurerm_resource_group.rsg]
+}
+
+resource "azurerm_storage_account" "sta" {
+  name                      = local.sta_name
+  resource_group_name       = azurerm_resource_group.rsg.name
+  location                  = var.location
+  account_kind              = "StorageV2"
+  account_tier              = "Premium"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
+
+  blob_properties{
+    delete_retention_policy {
+      days = var.delete_retention_days
+    }
+  }
+
+  tags = {
+    cost_center     = azurerm_resource_group.rsg.tags["cost_center"]
+    product         = azurerm_resource_group.rsg.tags["product"]
+    channel       = var.channel
+    description   = var.description
+    tracking_code = var.tracking_code
+    cia           = var.cia
+  }
+
+  depends_on = [azurerm_resource_group.rsg]
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = local.sta_name
+  container_access_type = "private"
+
+  depends_on = [azurerm_storage_account.sta]
 }
--- variables.tf
+++ variables.tf
@@ -14,10 +14,78 @@ variable "tenant_id" {
   type = string
 }
 
-variable "name" {
+variable "workload_acronym" {
   type        = string
   description = "(Required) Specifies the name of the Resource Group. Changing this forces a new resource to be created"
 }
+
+variable "channel" {
+  type = string
+  description = "(Optional) Distribution channel to which the associated resource belongs to."
+  default     = ""
+}
+
+variable "description" {
+  type        = string
+  description = "(Required) Provide additional context information describing the resource and its purpose."
+}
+
+variable "tracking_code" {
+  type        = string
+  description = "(Required) Allow this resource to be matched against internal inventory systems."
+}
+
+variable "sequence_number" {
+  type = string
+  description = "(Required) Sequence number for resources in WL."
+}
+
+variable "akv_sequence_number" {
+  type        = string
+  description = "(Optional) Key Vault sequence number."
+  default     = ""
+}
+
+variable "sta_sequence_number" {
+  type        = string
+  description = "(Optional) Storage Account sequence number."
+  default     = ""
+}
+
+variable "akv_key_sequence_number" {
+  type        = string
+  description = "(Optional) AKV Key sequence number which will be used to encrypt the Storage Account."
+  default     = ""
+}
+
+variable "ip_rules" {
+  type        = list
+  description = "(Optional) The ranges of IPs to can access Key Vault"
+  default = []
+}
+
+variable "virtual_network_subnet_ids" {
+  type        = list
+  description = "(Required) The Azure subnets that can access Key Vault"
+  default = []
+}
+
+variable "delete_retention_days" {
+  type        = number
+  description = "(Optional) Specifies the number of days that the blob should be retained, between 1 and 365 days. Defaults to 7"
+  default     = 7
+}
+
+variable "sta_tier" {
+  type        = string
+  description = "(Required) Storage account tier. Possible values: standard."
+}
+
+variable "sta_replication" {
+  type        = string
+  description = "(Required) Storage account replication policy. Possible values: LRS, GRS, RAGRS or ZRS "
+}
+
 variable "location" {
   type        = string
   description = "(Required) Specifies the supported Azure location where the Resource Group exists. Changing this forces a new resource to be created."
