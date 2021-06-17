96e42d7c7dbc6a3a4bd461dc94cfd68b833e3cfe
Jorge <8963841+georgevazj@users.noreply.github.com>
2021-06-17 15:37:37 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "rsg_name" {
 output "rsg_id" {
   value = azurerm_resource_group.rsg.id
 }
+
+output "kvt_name"{
+  value = azurerm_key_vault.akv.name
+}
+
+output "kvt_id"{
+  value = azurerm_key_vault.akv.id
+}
+
+output "storage_account_id" {
+  value = azurerm_storage_account.sta.id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sta.name
+}
